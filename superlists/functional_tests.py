1bb70d64b008d3dbfaf848cdf791c028351fb36e
wllwsc <wllwsc@126.com>
2016-02-13 08:56:26 +0800

    before refactor

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -24,11 +24,18 @@ class NewVisitorTest(unittest.TestCase):
 		inputbox.send_keys('Buy peacock feathers')
 		inputbox.send_keys(Keys.ENTER)
 
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox.send_keys('Use peacock feathers to make a fly')
+		inputbox.send_keys(Keys.ENTER)
+
+
 		table = self.browser.find_element_by_id('id_list_table')
 		rows = table.find_elements_by_tag_name('tr')
-		self.assertTrue(
-			any(rows.text == '1: Buy peacock feathers' for row in rows)
-			)
+		#self.assertTrue(
+		#	any(rows.text == '1: Buy peacock feathers' for row in rows)
+		#	)
+		self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+		self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
 
 		self.fail('Finish the test!')
 
--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -17,3 +17,22 @@ class HomePageTest(TestCase):
 		expected_html = render_to_string('home.html')
 
 		self.assertEqual(response.content.decode(), expected_html)
+
+	def test_home_page_can_save_a_POST_request(self):
+		request = HttpRequest()
+		request.method = 'POST'
+		request.POST['item_text'] = 'A new list item'
+
+		response = home_page(request)
+
+		self.assertIn('A new list item', response.content.decode())
+		expected_html = render_to_string('home.html', 
+			{'new_item_text': 'A new list item'}
+			)
+		#self.assertEqual(response.content.decode(), expected_html)
+
+		#self.assertTrue(
+		#	any(row.text == '1: Buy peacock feathers' for row in rows),
+		#	"New to-do item did not appear in table -- its text was:\n%s" %(table.text,)
+		#	)
+		
