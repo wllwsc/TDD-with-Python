c2c8a8aaee49f10d45cfe50f110bee0dde470e84
wllwsc <wllwsc@126.com>
2016-02-21 10:49:19 +0800

    before refactor when we can only deal with one item input

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -14,21 +14,27 @@ class NewVisitorTest(unittest.TestCase):
 		self.browser.get("http://localhost:8000")
 
 		self.assertIn('To-Do', self.browser.title)
-
 		header_text = self.browser.find_element_by_tag_name('h1').text
 		self.assertIn('To-Do', header_text)
 
 		inputbox = self.browser.find_element_by_id('id_new_item')
 		self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
 
-		inputbox.send_keys('Buy peacock feathers')
+		#inputbox.send_keys('Buy peacock feathers')
+		#inputbox.send_keys(Keys.ENTER)
+
+		inputbox.send_keys('Use peacock feathers to make a fly')
 		inputbox.send_keys(Keys.ENTER)
 
 		table = self.browser.find_element_by_id('id_list_table')
 		rows = table.find_elements_by_tag_name('tr')
-		self.assertTrue(
-			any(rows.text == '1: Buy peacock feathers' for row in rows)
-			)
+		#self.assertTrue(
+		#	any(row.text == '1: Buy peacock feathers' for row in rows),
+		#	"New to-do item did not appear in table -- it's text was:\n%s" %(table.text, )
+		#	)
+		
+		self.assertIn('1:Buy peacock feathers', [row.text for row in rows])
+		self.assertIn('2:Use peacock feathers to make a fly', [row.text for row in rows])
 
 		self.fail('Finish the test!')
 
