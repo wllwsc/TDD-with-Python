0f1f3f22080906eef2ee5e54c884772abd577c29
wllwsc <wllwsc@126.com>
2016-02-23 18:58:41 +0800

    make ft an app,use liveservertestcase

--- superlists/fts/test.py
+++ superlists/fts/test.py
@@ -1,8 +1,8 @@
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import unittest
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
 	def setUp(self):
 		self.browser = webdriver.Firefox()
 		self.browser.implicitly_wait(3)
@@ -16,7 +16,7 @@ class NewVisitorTest(unittest.TestCase):
 		self.assertIn(row_text, [row.text for row in rows])
 
 	def test_can_start_a_list_and_retrieve_it_later(self):
-		self.browser.get("http://localhost:8000")
+		self.browser.get(self.live_server_url)
 
 		self.assertIn('To-Do', self.browser.title)
 		header_text = self.browser.find_element_by_tag_name('h1').text
@@ -47,5 +47,3 @@ class NewVisitorTest(unittest.TestCase):
 		self.fail('Finish the test!')
 
 
-if __name__ == '__main__':
-	unittest.main(warnings='ignore')
